e723c5071758dff9938080d1cdcb82f589ec676f
Stanislav <gamongara@gmail.com>
2022-04-15 16:06:06 +0300

    names for env chages

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,12 @@ import { ErrorMiddleware } from "./Middlewares/ErrorMiddleware.js";
 const app = express();
 app.use(express.json());
 app.use(express.static(path.resolve() + "/Static"));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(cookieParser());
 app.use("/auth", AuthRouter);
 app.use("/users", UserRouter);
